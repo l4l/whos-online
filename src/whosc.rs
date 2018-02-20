7b02da56f0b5cc469175960ae6e7314c19163e90
Kitsu <mail@kitsu.me>
2018-02-20 19:35:25 +0300

    Remove silent peers from whosc cache

--- src/whosc.rs
+++ src/whosc.rs
@@ -1,30 +1,78 @@
 #![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 extern crate rocket;
-#[macro_use]
 extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
 
-use std::env;
 use std::sync::Mutex;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::time::Instant;
 
 use rocket::State;
 use rocket_contrib::Json;
 
 mod status;
 
+const OBSOLETE: u64 = 100;
+
+#[derive(Eq)]
+struct Update {
+    appear: Instant,
+    id: status::ID,
+}
+
+impl Update {
+    fn new(id: status::ID) -> Update {
+        Update {
+            appear: Instant::now(),
+            id: id,
+        }
+    }
+}
+
+impl Ord for Update {
+    fn cmp(&self, other: &Update) -> Ordering {
+        self.appear.cmp(&other.appear)
+    }
+}
+
+impl PartialOrd for Update {
+    fn partial_cmp(&self, other: &Update) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Update {
+    fn eq(&self, other: &Update) -> bool {
+        self.appear == other.appear
+    }
+}
+
 type Cache = Mutex<status::Map>;
+type Recency = Mutex<BinaryHeap<Update>>;
 
 #[post("/", format = "application/json", data = "<message>")]
-fn update(message: Json<status::TogglResponse>, map: State<Cache>) {
-    let mut locked_map = map.lock().expect("can't lock the map");
-    locked_map.insert(message.id.to_owned(), message.copy_data().unwrap());
+fn update(message: Json<status::TogglResponse>, cache: State<Cache>, recency: State<Recency>) {
+    let mut map = cache.lock().expect("can't lock the map");
+    let mut rec = recency.lock().expect("can't lock the recency");
+    let ref id = message.id;
+    map.insert(id.to_owned(), message.copy_data().unwrap());
+    rec.push(Update::new(id.to_owned()));
 }
 
 #[get("/", format = "application/json")]
-fn fetch(map: State<Cache>) -> Json<status::Map> {
-    let v = map.lock().expect("can't lock the map").clone();
+fn fetch(cache: State<Cache>, recency: State<Recency>) -> Json<status::Map> {
+    let mut map = cache.lock().expect("can't lock the map");
+    let mut rec = recency.lock().expect("can't lock the recency");
+    for i in rec.drain().rev().take_while(|x| {
+        x.appear.elapsed().as_secs() >= OBSOLETE
+    })
+    {
+        map.remove(&i.id);
+    }
+    let v = map.clone();
     Json(v)
 }
 
@@ -32,5 +80,6 @@ fn main() {
     rocket::ignite()
         .mount("/", routes![fetch, update])
         .manage(Cache::new(status::Map::new()))
+        .manage(Recency::new(BinaryHeap::new()))
         .launch();
 }
