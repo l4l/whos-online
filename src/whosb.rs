5471392b1f02d38cf679ca730eab41a1176c522e
Kitsu <mail@kitsu.me>
2018-02-21 12:31:20 +0300

    Refactor whosb

--- src/whosb.rs
+++ src/whosb.rs
@@ -17,16 +17,25 @@ use std::env;
 
 mod status;
 
+const BOT_TOKEN: &'static str = "TG_BOT_TOKEN";
+const DEFAULT_HOST: &'static str = "http://127.0.0.1:8080";
+const NO_USERS: &'static str = "No users found";
+const FETCH_ERROR: &'static str = "Nothing found";
+
+fn fetch(host: &str) -> Option<status::Map> {
+    reqwest::get(host)
+        .and_then(|mut x| x.text())
+        .ok()
+        .and_then(|resp| serde_json::from_str(&resp).ok())
+}
+
 fn main() {
     let mut lp = Core::new().unwrap();
-    let bot = bot::RcBot::new(lp.handle(), &env::var("TG_BOT_TOKEN").unwrap()).update_interval(200);
+    let bot = bot::RcBot::new(lp.handle(), &env::var(BOT_TOKEN).unwrap()).update_interval(200);
 
     let handle = bot.new_cmd("/ask").and_then(|(bot, msg)| {
-        let text = reqwest::get("http://127.0.0.1:8080")
-            .and_then(|mut x| x.text())
-            .ok()
-            .and_then(|resp| serde_json::from_str(&resp).ok())
-            .map(|m: status::Map| {
+        let text = fetch(DEFAULT_HOST)
+            .map(|m| {
                 m.into_iter()
                     .map(|(k, v)| {
                         let data_prnt = v.map(|d| format!("online [{}]", d.description))
@@ -35,7 +44,12 @@ fn main() {
                     })
                     .collect()
             })
-            .unwrap_or("nothing found".to_string());
+            .map(|m: String| if m.is_empty() {
+                NO_USERS.to_string()
+            } else {
+                m
+            })
+            .unwrap_or(FETCH_ERROR.to_string());
         bot.message(msg.chat.id, text).send()
     });
 
